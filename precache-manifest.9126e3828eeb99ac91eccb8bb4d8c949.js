946e0bae30e1884be7e35b61ecbde18f3c9355ee
Shibly <shibly2016@gmail.com>
2020-05-29 16:39:55 +0530

    Updates

---
+++ precache-manifest.9126e3828eeb99ac91eccb8bb4d8c949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c02b5336fe63c898a85e2c91429a67",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "ca170585ef1c27067b3f",
+    "url": "/quote-machine/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ad41a8453aa845fe8ed5",
+    "url": "/quote-machine/static/css/main.826f3450.chunk.css"
+  },
+  {
+    "revision": "ca170585ef1c27067b3f",
+    "url": "/quote-machine/static/js/2.f87e80e2.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/quote-machine/static/js/2.f87e80e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad41a8453aa845fe8ed5",
+    "url": "/quote-machine/static/js/main.ddc0465e.chunk.js"
+  },
+  {
+    "revision": "cbc9f74f0ef17ee9ad82",
+    "url": "/quote-machine/static/js/runtime-main.6752162c.js"
+  }
+]);
--- src/componants/button1.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const button1 = props => (
-   <button onClick={props.click} >{props.buttonName}</button>
-);
-
-export default button1;
